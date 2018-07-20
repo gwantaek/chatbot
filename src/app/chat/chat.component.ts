59b39faefe78786e3e60187f95454cd5b402a100
Gwan-Taek <lgt@hyundai-autoever.com>
2018-07-20 13:35:23 +0900

    DialogFlow Javascript client 적용

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,5 +1,9 @@
 import {Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren} from '@angular/core';
-import { MatDialog, MatList, MatListItem, MatIcon } from '@angular/material';
+import { MatDialog, MatList, MatListItem } from '@angular/material';
+
+import {ApiAiClient} from 'api-ai-javascript/es6/ApiAiClient';
+
+import {Message} from './shared/model/message';
 
 @Component({
   selector: 'app-chat',
@@ -8,6 +12,11 @@ import { MatDialog, MatList, MatListItem, MatIcon } from '@angular/material';
 })
 export class ChatComponent implements OnInit {
 
+  private apiAiClient: ApiAiClient;
+  private isListening = false;
+
+  messages: Message[] = [];
+  messageContent: string;
 
   // getting a reference to the overall list, which is the parent container of the list items
   @ViewChild(MatList, { read: ElementRef }) matList: ElementRef;
@@ -15,9 +24,20 @@ export class ChatComponent implements OnInit {
   // getting a reference to the items/messages within the list
   @ViewChildren(MatListItem, { read: ElementRef }) matListItems: QueryList<MatListItem>;
 
-  constructor(public dialog: MatDialog) { }
+  constructor() { }
 
   ngOnInit() {
+    const accessToken = '6cc610f8989b4d4bb7822bf628482eb7';
+    this.apiAiClient = new ApiAiClient({accessToken: accessToken});
   }
 
+  sendMessage(message: string): void {
+    if (!message) {
+      return;
+    }
+
+    this.apiAiClient.textRequest('오토에버').then((response) => {
+      console.log(response);
+    });
+  }
 }
